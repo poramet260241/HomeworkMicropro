b331c71e40b61f9ac43272ddaf47375e860ab4dc
poramet260241 <oblivion2408@gmail.com>
2020-03-09 07:21:49 +0700

    add chapter 7

---
+++ chapter7/q8_code/q8_code/main.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#define F_CPU 1000000UL
+#include <util/delay.h>
+#define inc 0
+#define dec 1
+#define nothing 2
+
+unsigned char TB7SEG[] = {
+	
+}
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
